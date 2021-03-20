efeb59b9ced93f9726594ff07d1e17a9a3d9a955
eduardonk9999 <thiago.novak7@gmail.com>
2021-03-20 16:03:00 -0300

    Add, projeto

---
+++ src/components/Produto.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3></h3>
+    <h2>Meu produto</h2>
+    <p>Bla bla bla</p>
+    <input type="text" placeholder="Preço">
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
